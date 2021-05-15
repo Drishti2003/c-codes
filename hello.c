67ec87219b85c1567fa9c0d8b1981d43dc171c9f
Rishabh Kirodiwal <r.rishabhkirodiwal@gmail.com>
2021-05-15 19:45:30 +0530

    hello file added

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("Drishti Kirodiwal\n");
+    printf("Drishti\nKirodiwal\n"); // for next line
+	printf("Drishti\tdirodiwal\n"); // for space
+	printf("Drishti\bKirodiwal\n"); // backspace
+	printf("Drishti\\Kirodiwal\n"); // put the quote 
+	printf("Drishti\"Kirodiwal\n"); // print "
+	printf("shree\tDrishti\rKirodiwal"); // remove all the characters before \r
+
+}
