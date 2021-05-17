dc7fcd989d097bb0b79bfde42894ceb3b19f82d2
Drishti2003 <s.sainibabli@gmail.com>
2021-05-17 18:00:38 +0530

    arith2

---
+++ arithmetic2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	x=2+7;
+	printf("%d\n",x);
+	/* 3/4     = 0
+	   3.0/4   = 0.75
+	   3/4.0   = 0.75
+	   3.0/4.0 = 0.75
+	*/
+	x=23%5;
+	printf("%d\n",x);
+
+	x=5>4>3;
+	printf("%d\n",x);
+}
