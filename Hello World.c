f30720517ec314f97dad3cce2146ac55688ceb87
Drishti2003 <s.sainibabli@gmail.com>
2021-05-21 21:22:44 +0530

    Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{char s[100],i;
+    
+    gets(s);
+    
+    printf("Hello, World!\n");
+    puts(s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	
+}
