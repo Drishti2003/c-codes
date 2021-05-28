a9811e53e31263bd9ad2a766d180f51f86018ec3
Drishti2003 <s.sainibabli@gmail.com>
2021-05-28 13:30:55 +0530

    Swap_numbers.c

---
+++ Swap_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int*,int*);
+int main()
+{	int a,b;
+	printf("Enter two numbers\n");
+	scanf("%d%d",&a,&b);
+	swap(&a,&b);
+	printf("a=%d b=%d",a,b);
+}
+void swap(int *x,int *y)
+{	int t;
+	t=*x;
+	*x=*y;
+	*y=t;
+}
