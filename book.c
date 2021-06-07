4386c4c953347c3f3ac1f8a8ae406955f50be524
Drishti2003 <s.sainibabli@gmail.com>
2021-06-07 12:49:21 +0530

    book.c

---
+++ book.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+struct book
+{
+	int bookid;
+	char title[20];
+	float price;
+};
+struct book input()
+{
+	struct book b;
+	printf("Enter bookid, title and price");
+	scanf("%d",&b.bookid);
+	scanf("%c",&b.title);
+	scanf("%f",&b.price); 
+	return(b);
+}
+void display(struct book b)
+{
+	printf("\n %d %s %f",b.bookid,b.title,b.price);
+} 
+void main()
+{
+	struct book b1;
+	b1=input();
+	display(b1);
+}
