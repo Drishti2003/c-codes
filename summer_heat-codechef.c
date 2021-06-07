f021ce176755f2551b598a87eca6fb9c8ece1c17
Drishti2003 <s.sainibabli@gmail.com>
2021-06-07 12:54:14 +0530

    summer_heat-codechef.c

---
+++ summer_heat-codechef.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int x,z;
+	scanf("%d",&x);
+	while(x--)
+ { int a,b,c,d;
+ 	int x,y,z;
+ 	scanf("%d %d %d %d",&a,&b,&c,&d);
+ 	x=c/a;
+ 	y=d/b;
+ 	z=x+y;
+ 	
+ }
+printf("%d\n",z);
+}
