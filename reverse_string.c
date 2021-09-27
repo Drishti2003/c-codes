2fdc74d328a26f3ca8b69dac6cfeef59f3d98956
Drishti2003 <s.sainibabli@gmail.com>
2021-09-27 18:17:37 +0530

    reverse_string.c

--- reverse_string.c
+++ reverse_string.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+ 
 int main()
-{	 char s[1000],r[1000];
-	int end,begin,count=0;
-	printf("Enter the string");
-	gets(s);
-	while(s[count] != \0)
-	
-	count++;
-	
-	
-	for(begin=0;begin<=count;begin++)
-	{
-		r[begin]=s[end];
-		end--;
-	}
-	printf("%s",r);
+{
+  	char Str[100], RevStr[100];
+  	int i, j, len;
+ 
+  	printf("\n Please Enter any String :  ");
+  	gets(Str);
+  	
+  	j = 0;
+  	len = strlen(Str);
+ 
+  	for (i = len - 1; i >= 0; i--)
+  	{
+  		RevStr[j++] = Str[i];
+  	}
+  	RevStr[i] = '\0';
+ 
+  	printf("\n String after Reversing = %s", RevStr);
+  	
+  	return 0;
 }
-
-
-
