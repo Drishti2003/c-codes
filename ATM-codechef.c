bc8bd13c899b44fd7ceb8152dde058d0784c141f
Drishti2003 <s.sainibabli@gmail.com>
2021-09-27 18:13:40 +0530

    ATM-codechef.c

--- ATM-codechef.c
+++ ATM-codechef.c
@@ -4,16 +4,19 @@ int main()
 	float a,b;
 	scanf("%d %f",&x,&a);
 	if(x<a)
-	{ if(x%5==0)
-	{ b=a-x-.50;
-		printf("%.2f\n",b);
-	}
-	else
 	{ 
-		printf("%.2f\n",a);
-	}
+		if(x%5==0)
+		{ 
+			b=a-x-.50;
+			printf("%.2f\n",b);
+		}
+	else
+		{ 
+			printf("%.2f\n",a);
+		}
 	}
 	else 
-	{ printf("%.2f\n",a);	
+	{ 
+		printf("%.2f\n",a);	
 	}
 }
