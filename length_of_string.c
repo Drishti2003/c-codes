7d0205ab35fc8887e662f4a49e50b60eb100d398
Drishti2003 <s.sainibabli@gmail.com>
2021-09-27 18:17:15 +0530

    length_of_string.c

--- length_of_string.c
+++ length_of_string.c
@@ -16,11 +16,10 @@ char* reverse(char *p)
 {	int l,i;
 	char t;
 	for(l=0;*(p+l)!='\0';l++);
-	printf("%d",i);
-	/*for(i=0;i<l/2;i++)
+	for(i=0;i<l/2;i++)
 	{	t=*(p+i);
 		*(p+i)=*(p+l-1-i);
 		*(p+l-1-i)=t;
-	}*/
+	}
 	return(p); 
 }
