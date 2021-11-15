1a5c81ba77fa901a6f8bf40b7e68724e18e08ca8
Drishti2003 <s.sainibabli@gmail.com>
2021-11-15 19:12:32 +0530

    test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char ch;
+	FILE *fp;
+	fp=fopen("f1.txt","r");
+	if(fp==NULL)
+	{	
+		printf("File Not found\n");
+		exit(1);
+	}
+	ch=fgetc(fp);
+	while(!feof(fp))
+	{
+		printf("%c",ch);
+		ch=fgetc(fp);
+	}
+	fclose(fp);
+}
