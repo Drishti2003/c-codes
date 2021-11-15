d012f0b8883809eaff7b5a52d27577b4b7bc0f79
Drishti2003 <s.sainibabli@gmail.com>
2021-11-15 19:11:23 +0530

    file.c

--- file.c
+++ file.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main()
 {
 	int i; 
 	FILE *fp;
-	char s[]="Hello Students";
+	char s[100];
 	fp=fopen("f1.txt","w");
 	if(fp==NULL) {
 		printf("File cannot open");
 		exit(1);
 	}
+	printf("Enter a string\n");
+	gets(s);
 	for(i=0;i<strlen(s);i++)
 	fputc(s[i],fp);
 	fclose(fp);
